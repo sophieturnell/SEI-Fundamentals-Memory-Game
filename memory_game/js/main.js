fe23ea0849941d9f6e33bae51237a3ce59fa3182
Sophie Turnell <sophAW@Sophies-Air.home>
2019-08-18 14:29:22 +0100

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
+//save changes to main.js
