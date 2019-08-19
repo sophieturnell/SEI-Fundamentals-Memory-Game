63e3aeb1bf1ab20cea1d041709b18302410547ac
Sophie Turnell <sophAW@Sophies-Air.home>
2019-08-19 19:36:54 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,6 @@
 console.log("Up and running!");
 
+/*
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
@@ -7,4 +8,28 @@ var cardFour = "king";
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
-//save changes to main.js
+//save changes to main.js*/
+
+const cards = ["queen", "queen", "king", "king"];
+
+const cardsInPlay = [ ];
+
+const cardOne = cards[0];
+
+cardsInPlay.push("cardOne");
+
+console.log("User flipped " + cardOne);
+
+const cardTwo = cards[2];
+
+cardsInPlay.push("cardTwo");
+
+console.log("User flipped " + cardTwo);
+
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You've found a match!");
+}
+else {
+	alert("Sorry, try again.")
+}
