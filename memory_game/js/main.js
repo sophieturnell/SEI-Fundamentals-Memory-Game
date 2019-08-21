3268b284aba85df4862fc7bb4b84660f60665a06
Sophie Turnell <sophAW@Sophies-Air.home>
2019-08-21 09:19:43 +0100

    commit game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -11,25 +11,48 @@ console.log("User flipped " + cardThree);
 //save changes to main.js*/
 
 const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
 
-const cardsInPlay = [ ];
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+ 	 	console.log("You found a match!");
+	} else {
+	  console.log("Sorry, try again.");
+	}
+};	
+
+var flipCard = function (cardId) {
+
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+};
+
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
+
+//function that will store all steps related to selecting, or flipping over, a card
+
+//function to group together the logic to check to see if the two cards that the user has flipped over match each other
+
+
+
+/*Previous code
 
 const cardOne = cards[0];
+	cardsInPlay.push("cardOne");
+	console.log("User flipped " + cardOne);
 
-cardsInPlay.push("cardOne");
-
-console.log("User flipped " + cardOne);
-
-const cardTwo = cards[2];
-
-cardsInPlay.push("cardTwo");
-
-console.log("User flipped " + cardTwo);
+	const cardTwo = cards[2];
+	cardsInPlay.push("cardTwo");
+	console.log("User flipped " + cardTwo);
 
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You've found a match!");
-}
-else {
-	alert("Sorry, try again.")
-}
+	if (cardsInPlay.length === 2 && cardOne == cardTwo) {
+		alert("You've found a match!");
+	}
+	else {
+		alert("Sorry, try again.")
+	}*/
+
